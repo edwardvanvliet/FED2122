abfdfadcf10fc60df68eea0d7fac71b7f101172e
Edward van Vliet <94113748+edwardvanvliet@users.noreply.github.com>
2021-12-03 13:46:31 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+var hamMenu = document.querySelector("nav ul");
+
+var hamButton = document.querySelector(".icon");
+
+
+
+// Klik op menu Button
+
+hamButton.addEventListener("click", toggleHamburgerMenu);
+
+
+
+function toggleHamburgerMenu(){
+
+    hamMenu.classList.toggle("...");
+
+}
